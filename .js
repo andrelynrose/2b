6809862fbaf5b6f178825030752c6dc783b820c6
ANDRELYN <andrelynroseg@gmail.com>
2025-03-12 17:14:15 +0800

    pangetkram

---
+++ .js
@@ -0,0 +1,29 @@
+function convert() {
+    let unitType = document.getElementById("unitType").value;
+    let inputValue = parseFloat(document.getElementById("inputValue").value;
+    let result = document.getElementById("result);
+    
+    if (isNaN(inputValue)){
+        result.textContent = "0";
+        return;
+    }
+    if(unitType === "meter-kilometer"){
+        result.textContent = (inputValue/1000).toFixed(4) + "km";
+    }else if (unitType === "inch-centimeter") {
+        result.textContent = (inputValue * 2.54) toFixed(2) +_"cm";
+    }
+}
+//Update input placeholder based on selection
+function updatePlaceholder(){
+    let unitType = document.getElementById("unitType").value;
+    let inputField = document.getElementById ("inputvalue");
+
+    if (inputType = "meter-kilometer"){
+        inputField. placeholder =" Enter meters"; 
+    }else{
+        inputField.placeholder ="Enter inches";
+    }
+}
+
+    
+
